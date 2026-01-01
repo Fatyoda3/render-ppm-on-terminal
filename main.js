415e88dcb7f1aa5a1de13fb1b648deaf2a31a429
fatyoda3 <shivangsingh654@gmail.com>
2026-01-01 14:34:25 +0530

    some more

--- main.js
+++ main.js
@@ -4,10 +4,15 @@ import { displayImage } from "./src/image_processor.js";
 const KIRBY = "./data/kirby/kirby.ppm";
 const SCENE = "./data/test/scene.ppm";
 
-const main = async () => {
-  const { binPath, metaPath } = await writeBinaryAndHeader(SCENE);
+const images = [KIRBY, SCENE];
 
+const drawOneImage = async (imagePath) => {
+  const { binPath, metaPath } = await writeBinaryAndHeader(imagePath);
   await displayImage(binPath, metaPath);
+};
+const main = async () => {
+  await drawOneImage(SCENE);
+  await drawOneImage(KIRBY);
 };
 
 main();
